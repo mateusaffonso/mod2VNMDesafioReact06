a7acea979681bf6fea3d6ebed5cf4f27b63eed54
mateusaffonso <augustomateus@poli.ufrj.br>
2022-02-12 21:00:04 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    number: 0
+
+  }
+
+  score = () => {
+    
+    this.setState ({
+      number: this.state.number <= 9 ? this.state.number + 1 : 0
+    })
+    }
+  
+
+
+  render() {
+    return(
+      <div className="containerOn">
+        <div className="containerIn">
+
+        <h1>Contador Vai na Web</h1>
+        <p><strong>{this.state.number}</strong></p>
+        <button onClick={this.score}>Contar</button>
+
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
